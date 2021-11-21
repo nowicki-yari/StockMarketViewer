d198578fb89e6a83d878acfe963f606fbb0a61dc
nowicki-yari <yari.nowicki@outlook.com>
2021-11-21 18:58:33 +0100

    added php fetch for python api

--- PythonStockAPI/StockData.py
+++ PythonStockAPI/StockData.py
@@ -2,8 +2,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from StockInfo import StockInfo
 from StockHistoricData import StockHistoricData
+from flask_cors import CORS
+
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 api.add_resource(StockInfo, '/stock/<string:stock>/info')
 api.add_resource(StockHistoricData, '/stock/<string:stock>/history/<string:start_date>/<string:end_date>')
--- resources/views/stocks.blade.php
+++ resources/views/stocks.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div id="stock_list">
         @foreach($stocks as $s)
-            <div id="stock">
+            <div id="stock" onclick="div_stock_click({{$s}})">
                 <b>{{$s->name}}</b>
                 <br>
                 <p>{{$s->symbol}}</p>
