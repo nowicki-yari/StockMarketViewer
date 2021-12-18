655cd8f4da1d541bbfc7594bb77c252d68159b07
nowicki-yari <yari.nowicki@outlook.com>
2021-12-18 14:03:48 +0100

    added migration

---
+++ database/migrations/2021_12_18_130113_alter_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('favorite_1')->nullable();
+            $table->string('favorite_2')->nullable();
+            $table->string('favorite_3')->nullable();
+            $table->string('favorite_4')->nullable();
+            $table->string('favorite_5')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
